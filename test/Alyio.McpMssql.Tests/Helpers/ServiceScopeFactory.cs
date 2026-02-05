8bf275cc0657c81fab6c9d64010add07b60e16e4
Jon X <ousiax@hotmail.com>
2026-02-05 19:07:07 +0800

    refactor: Apply consistent namespace formatting in ServiceScopeFactory

--- test/Alyio.McpMssql.Tests/Helpers/ServiceScopeFactory.cs
+++ test/Alyio.McpMssql.Tests/Helpers/ServiceScopeFactory.cs
@@ -4,36 +4,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Alyio.McpMssql.Tests.Helpers
-{
+namespace Alyio.McpMssql.Tests.Helpers;
+
 /// <summary>
 /// Factory for creating service scopes in tests.
 /// </summary>
-    internal static class ServiceScopeFactory
+internal static class ServiceScopeFactory
+{
+    private static readonly IServiceScopeFactory s_factory;
+
+    static ServiceScopeFactory()
     {
-        private static readonly IServiceScopeFactory s_factory;
+        var builder = Host.CreateApplicationBuilder();
 
-        static ServiceScopeFactory()
-        {
-            var builder = Host.CreateApplicationBuilder();
+        builder.Configuration.AddUserSecrets(typeof(ServiceScopeFactory).Assembly);
 
-            builder.Configuration.AddUserSecrets(typeof(ServiceScopeFactory).Assembly);
+        builder.Services.AddMcpMssqlOptions(builder.Configuration);
 
-            builder.Services.AddMcpMssqlOptions(builder.Configuration);
+        var host = builder.Build();
 
-            var host = builder.Build();
+        s_factory = host.Services.GetRequiredService<IServiceScopeFactory>();
+    }
 
-            s_factory = host.Services.GetRequiredService<IServiceScopeFactory>();
-        }
-
-        /// <summary>
-        /// Creates a new service scope.
-        /// </summary>
-        /// <returns>A new <see cref="IServiceScope"/>.</returns>
-        public static IServiceScope Create()
-        {
-            return s_factory.CreateScope();
-        }
+    /// <summary>
+    /// Creates a new service scope.
+    /// </summary>
+    /// <returns>A new <see cref="IServiceScope"/>.</returns>
+    public static IServiceScope Create()
+    {
+        return s_factory.CreateScope();
     }
 }
 
